38c8d030256df5dc05a4a8318b5c13c8ffa9e8ba
keape <keape86@gmail.com>
2026-06-02 23:23:35 +0200

    Revert "feat: add activity_overrides jsonb column to piante_coltivate"
    This reverts commit fdf8e7c98047b5cdbf7fcff39a506222a27f3369.

--- supabase/migrations/20260602000001_activity_overrides.sql
+++
@@ -1,4 +0,0 @@
--- Migration: aggiunge colonna activity_overrides a piante_coltivate
--- Struttura: [{"nome": "Irrigazione", "recurrence_days": 4}, {"nome": "Raccolta", "offset_days": 85}]
-alter table piante_coltivate
-  add column activity_overrides jsonb;
